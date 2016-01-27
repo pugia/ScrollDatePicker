c312853f7e7e80e188dd5f195fc712c3f8470c65
Marco Pugliese <marco.pugliese@pugia.com>
2016-01-27 23:57:51 +0100

    Scrolling

--- ScrollDatePicker/Picker/Picker.swift
+++ ScrollDatePicker/Picker/Picker.swift
@@ -12,9 +12,22 @@ import UIKit
 
     var view:UIView!
     var nibName:String = "Picker"
+
+    var date = NSDate()
+
     @IBOutlet weak var dayNumbers: ScrollerNumbersCollectionView!
     @IBOutlet weak var monthNumbers: ScrollerNumbersCollectionView!
     @IBOutlet weak var yearNumbers: ScrollerNumbersCollectionView!
+    @IBOutlet weak var hourNumbers: ScrollerNumbersCollectionView!
+    @IBOutlet weak var minuteNumbers: ScrollerNumbersCollectionView!
+
+    private var day:Int = 3
+    private var month:Int = 1
+    private var year:Int = 1986
+    private var hour:Int = 7
+    private var minute:Int = 15
+    
+    private let calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,6 +46,14 @@ import UIKit
         view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
         addSubview(view)
         scrollerSetup()
+        
+        let components = calendar!.components([.Year, .Month, .Day, .Hour, .Minute], fromDate: date)
+        day = components.day
+        month = components.month
+        year = components.year
+        hour = components.hour
+        minute = components.minute
+        
     }
     
     func loadViewFromNib() -> UIView {
@@ -45,23 +66,84 @@ import UIKit
     }
     
     func scrollerSetup() {
-        dayNumbers.initDataCell(popolateNumbers(1, to: 31), identifier: "DayNumbersCell")
+        
+        yearNumbers.initDataCell(popolateNumbers(1, to: 2050), identifier: "YearNumbersCell")
+        yearNumbers.callback = callbackPicker
+        dayNumbers.initDataCell(popolateDays(self.year, m: self.month), identifier: "DayNumbersCell")
+        dayNumbers.callback = callbackPicker
         monthNumbers.initDataCell(popolateMonths(), identifier: "MonthNumbersCell")
-        yearNumbers.initDataCell(popolateNumbers(1970, to: 2050), identifier: "YearNumbersCell")
+        monthNumbers.callback = callbackPicker
+        hourNumbers.initDataCell(popolateNumbers(0, to: 23), identifier: "HourNumbersCell")
+        minuteNumbers.initDataCell(popolateNumbers(0, to: 59), identifier: "MinuteNumbersCell")
+
+        _ = Timeout(0.1) {
+            self.scrollerInitValues()
+        }
+        
+    }
+    
+    func scrollerInitValues() {
+        self.dayNumbers.setCurrentElement(self.day)
+        self.monthNumbers.setCurrentElement(self.month)
+        self.yearNumbers.setCurrentElement(self.year)
+        self.hourNumbers.setCurrentElement(self.hour,offset: 1)
+        self.minuteNumbers.setCurrentElement(self.minute, offset: 1)
+    }
+    
+    func callbackPicker() {
+        year = self.yearNumbers.getCurrentElement()
+        month = self.monthNumbers.getCurrentElement()
+        day = self.dayNumbers.getCurrentElement()
+        hour = self.hourNumbers.getCurrentElement()
+        minute = self.minuteNumbers.getCurrentElement()
+        
+        let maxDay = getMonthDays(year, m: month)
+        if (day > maxDay) {
+            day = maxDay;
+        }
+        scrollerSetup()
+   
     }
     
     func popolateNumbers(from:Int, to:Int) -> Array<String> {
-        var out:Array<String> = []
-        out.append("")
+        
+        var out:Array<String> = [""]
         for index in from...to {
-            out.append("\(index)")
+            out.append(String(format: "%02d", index))
         }
         out.append("")
         return out
     }
     
     func popolateMonths() -> Array<String> {
+        
         return ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", ""]
+        
+    }
+    
+    func popolateDays(y:Int, m:Int) -> Array<String> {
+        
+        var out:Array<String> = [""]
+        for index in 1...getMonthDays(y, m: m) {
+            out.append(String(format: "%02d", index))
+        }
+        out.append("")
+        return out
+        
+    }
+    
+    func getMonthDays(y:Int, m:Int) -> Int {
+
+        let components = NSDateComponents()
+        components.year = y
+        components.month = m+1
+        components.day = 0
+        
+        let date = calendar!.dateFromComponents(components)
+        let components2 = calendar!.components([.Month, .Day], fromDate: date!)
+        
+        return components2.day
+        
     }
     
     /*
--- ScrollDatePicker/Picker/ScrollerNumbers.swift
+++ ScrollDatePicker/Picker/ScrollerNumbers.swift
@@ -13,8 +13,12 @@ import UIKit
 class ScrollerNumbersCollectionView: UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate, UICollectionViewDelegateFlowLayout {
     
     var cell_identifier = "Cell"
+    var callback: (Void -> Void)?
+    var cellHeight:CGFloat = 0
     
     private var data:Array<String> = []
+    private var ipsilon:CGFloat = 0
+    private var selected:Int = 1
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -22,10 +26,10 @@ class ScrollerNumbersCollectionView: UICollectionView, UICollectionViewDelegate,
         self.dataSource = self
         self.delegate = self
         self.registerClass(ScrollerNumbersCollectionViewCell.self, forCellWithReuseIdentifier: self.cell_identifier)
-        self.alwaysBounceVertical = false
+        self.alwaysBounceVertical = true
         self.showsVerticalScrollIndicator = false
         self.canCancelContentTouches = true
-        self.bounces = false
+        self.bounces = true
         
     }
     
@@ -46,20 +50,57 @@ class ScrollerNumbersCollectionView: UICollectionView, UICollectionViewDelegate,
         return cell
     }
     
+    override func touchesShouldCancelInContentView(view: UIView) -> Bool {
+        return true
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        self.ipsilon = round(scrollView.contentOffset.y / self.cellHeight) * self.cellHeight
+    }
+    
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        
+        print("scrollViewDidEndDecelerating")
+        self.ipsilon = round(scrollView.contentOffset.y / self.cellHeight) * self.cellHeight
+        _ = Timeout(0.2) {
+            self.fixCellPosition(scrollView)
+        }
     }
     
     func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        
+        print("scrollViewWillEndDragging")
+        if(velocity.y == 0) {
+            _ = Timeout(0.2) {
+                self.ipsilon = round(scrollView.contentOffset.y / self.cellHeight) * self.cellHeight
+                self.fixCellPosition(scrollView)
+            }
+        }
+    }
+
+    func fixCellPosition(scrollView: UIScrollView) {
+        UIView.animateWithDuration(0.1, animations: {
+            scrollView.contentOffset.y = self.ipsilon
+        })
+        self.selected = Int(self.ipsilon / self.cellHeight) + 1
+        _ = Timeout(0.1) {
+            self.callback?()
+        }
+    }
+
+    func setCurrentElement(i:Int, offset:Int=0) {
+        self.selected = i
+        self.contentOffset.y = self.cellHeight * CGFloat(i-1+offset)
+    }
+    
+    func getCurrentElement() -> Int {
+        return self.selected
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        if (self.cellHeight == 0) {
+            self.cellHeight = self.frame.height/3
+            print(self.cellHeight)
+        }
         return CGSize(width: self.frame.width, height: self.frame.height/3)
-    }
-    
-    override func touchesShouldCancelInContentView(view: UIView) -> Bool {
-        return true
     }
     
 }
@@ -78,11 +119,43 @@ class ScrollerNumbersCollectionViewCell: UICollectionViewCell {
         
         self.addSubview(label)
         
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
+}
+
+/// Timeout wrapps a callback deferral that may be cancelled.
+///
+/// Usage:
+/// Timeout(1.0) { println("1 second has passed.") }
+///
+class Timeout: NSObject
+{
+    private var timer: NSTimer?
+    private var callback: (Void -> Void)?
+    
+    init(_ delaySeconds: Double, _ callback: Void -> Void)
+    {
+        super.init()
+        self.callback = callback
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(delaySeconds),
+            target: self, selector: "invoke", userInfo: nil, repeats: false)
+    }
+    
+    func invoke()
+    {
+        self.callback?()
+        // Discard callback and timer.
+        self.callback = nil
+        self.timer = nil
+    }
+    
+    func cancel()
+    {
+        self.timer?.invalidate()
+        self.timer = nil
+    }
 }
